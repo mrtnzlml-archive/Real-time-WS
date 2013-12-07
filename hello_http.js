304d538eb98b4b9051bf11b799522b3cc8180383
mrtnzlml <mrtnzlml@gmail.com>
2013-12-07 19:49:26 +0100

    Converted indentation to tabs, improved pure js client, ws protocol.

--- hello_http.js
+++ hello_http.js
@@ -4,8 +4,8 @@ var redis = require('node-redis').createClient('80', '127.0.0.1');
 
 var index = fs.readFileSync(__dirname + '/index.html');
 var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(index);
 }).listen(1234);
 
 var devices = {};
@@ -15,21 +15,21 @@ var io = require('socket.io').listen(app);
 
 setInterval(function() {
 	var to = devices['SERVER'];
-    io.sockets.socket(to).emit('devices', devices);
+	io.sockets.socket(to).emit('devices', devices);
 }, 100);
 
 io.sockets.on('connection', function(socket) {
 
-    socket.on('register', function(uid) {
-    	socket.uid = uid;
-    	devices[uid] = socket.id;
-    	socket.emit('message', 'Registration OK, welcome ' + uid + ' (' + socket.id + ')');
-    	var to = devices['SERVER'];
-    	io.sockets.socket(to).emit('devices', devices);
-    });
+	socket.on('register', function(uid) {
+		socket.uid = uid;
+		devices[uid] = socket.id;
+		socket.emit('message', 'Registration OK, welcome ' + uid + ' (' + socket.id + ')');
+		var to = devices['SERVER'];
+		io.sockets.socket(to).emit('devices', devices);
+	});
 
-    socket.on('disconnect', function(){
-        delete devices[socket.uid];
-    });
+	socket.on('disconnect', function(){
+		delete devices[socket.uid];
+	});
 
 });
--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 var io = require("socket.io-client");
-client = io.connect("http://127.0.0.1:1234/");
+client = io.connect("ws://127.0.0.1:1234/");
 
-client.on('time', function(data) {
-    console.log(data.time);
+client.on('connect', function(){
+	client.emit('register', 'UID-0003');
+});
+
+client.on('message', function(data) {
+	console.log(data);
+});
+
+client.on('error', function() {
+	console.error(arguments)
 });
