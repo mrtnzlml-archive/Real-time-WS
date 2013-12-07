f2bc090f872855d01995f37b7a04bba83dcd9bab
mrtnzlml <mrtnzlml@gmail.com>
2013-12-07 21:32:33 +0100

    redis implemented

--- hello_http.js
+++ hello_http.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var redis = require('node-redis').createClient('80', '127.0.0.1');
+var redis = require('redis').createClient(); //https://github.com/mranney/node_redis
 
 var index = fs.readFileSync(__dirname + '/index.html');
 var app = http.createServer(function(req, res) {
@@ -12,11 +12,12 @@ var devices = {};
 
 // Socket.io server listens to our app
 var io = require('socket.io').listen(app);
+io.set('log level', 2); // reduce logging
 
-setInterval(function() {
+/*setInterval(function() {
 	var to = devices['SERVER'];
 	io.sockets.socket(to).emit('devices', devices);
-}, 100);
+}, 100);*/
 
 io.sockets.on('connection', function(socket) {
 
@@ -26,6 +27,14 @@ io.sockets.on('connection', function(socket) {
 		socket.emit('message', 'Registration OK, welcome ' + uid + ' (' + socket.id + ')');
 		var to = devices['SERVER'];
 		io.sockets.socket(to).emit('devices', devices);
+
+		/*redis.set('key', 'value', function(err, result) {
+			console.log(result);
+		});*/
+		//redis.bgsave(); //redis server needs admin permission
+		redis.get('key', function(err, result) {
+			console.log(result);
+		});
 	});
 
 	socket.on('disconnect', function(){
@@ -33,3 +42,5 @@ io.sockets.on('connection', function(socket) {
 	});
 
 });
+
+redis.on('error', function(err) { console.log(err); });
