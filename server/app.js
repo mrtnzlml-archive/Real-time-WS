7b04428d0841374876f7071e2afdbda68553e7de
mrtnzlml <mrtnzlml@gmail.com>
2014-01-02 21:41:41 +0100

    MVC update

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var redis = require('redis').createClient(); //https://github.com/mranney/node_redis
+var fs = require('fs');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -12,18 +12,37 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(express.methodOverride()); //support _method (PUT in forms etc)
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
+app.use(function (req, res, next) { //TODO: case sensitive URL (webalize?)
+    if (req.url.substr(-1) == '/' && req.url.length > 1) {
+        res.redirect(301, req.url.slice(0, -1));
+    } else if (req.url.substr(-6) == '/index') {
+        res.redirect(301, req.url.slice(0, -6)); //kanonizace
+    } else {
+        next();
+    }
+});
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
+fs.readdirSync(path.join(__dirname, 'controllers')).forEach(function (name) {
+    var obj = require(path.join(__dirname, 'controllers', name));
+    var method = 'get';
+    var url;
+    for (var key in obj) {
+        url = '/' + name + '/' + key;
+        url = url.replace(/\/index$/i, '');
+        url = url.replace(/homepage/i, '');
+        app[method](url, obj[key]); //routing
+    }
+});
 
-app.get('/', routes.index);
-app.get('/test', routes.test);
-app.get('*', routes.e404);
+app.use(function (err, req, res, next) { //error middleware
+    console.error(err.stack);
+    res.status(500).render('error/5xx');
+});
+app.use(function (req, res, next) {
+    res.status(404).render('error/404', { url: req.originalUrl });
+});
 
 var server = http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
@@ -61,7 +80,7 @@ io.sockets.on('connection', function (socket) {
 });
 
 setInterval(function () {
-    // NOP
+    //NOP
 }, 100);
 
 redis.on("error", function (err) {
---
+++ server/controllers/homepage/index.js
@@ -0,0 +1,7 @@
+exports.index = function (req, res) {
+    res.render('homepage/index');
+};
+
+exports.indexx = function (req, res) {
+    res.render('homepage/index');
+};
