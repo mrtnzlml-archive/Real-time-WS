c2d34e5c27d181d42de0e4cc8e1000bd0ca379ed
Martin Zlámal <mrtnzlml@gmail.com>
2014-11-06 22:03:56 +0100

    added start.bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+
+cd C:\Users\Martin\Desktop\real-time-ws\redis\bin\release\redis-2.8.17
+start redis-server.exe
+
+cd C:\Users\Martin\Desktop\real-time-ws\server\app\bin
+set DEBUG=myapp & node www
+
+exit
