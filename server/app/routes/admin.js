fefdb6425a7a65455d21b6284ddf7641a22c54bf
Martin Zlámal <mrtnzlml@gmail.com>
2014-11-25 23:21:24 +0100

    server routes update

--- server/app/routes/admin.js
+++ server/app/routes/admin.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET */
-router.get('/', function(req, res) {
-  res.render('api', {
-  	path: '/api'
-  });
+router.get('/', function (req, res) {
+	res.render('admin', {
+		path: '/admin'
+	});
 });
 
 module.exports = router;
