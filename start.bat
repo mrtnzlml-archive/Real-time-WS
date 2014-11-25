7232426078987c2f5ab84a58692d9a54f7e63575
Martin Zlámal <mrtnzlml@gmail.com>
2014-11-25 23:32:35 +0100

    server cleanup

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@
 cd C:\Users\Martin\Desktop\real-time-ws\redis\bin\release\redis-2.8.17
 start redis-server.exe
 
-cd C:\Users\Martin\Desktop\real-time-ws\server\app\bin
+cd C:\Users\Martin\Desktop\real-time-ws\server\bin
 set DEBUG=app & node www
 
 exit
