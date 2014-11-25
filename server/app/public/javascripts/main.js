6cc77a91f022039db5acd2690fd1b8ab3c321395
Martin Zlámal <mrtnzlml@gmail.com>
2014-11-25 19:02:02 +0100

    cleanup

--- server/app/public/javascripts/main.js
+++ server/app/public/javascripts/main.js
@@ -6,7 +6,8 @@ var socket = io.connect('http://' + IP + ':' + PORT);
 socket.on('data', function (data) {
 	var result = String(data).match(/\+([a-z]{4}_[0-9]{6}):(.*)/i);
 	if (result) {
-		document.getElementById(result[1]).innerHTML = result[1] + " : " + result[2];
+		//document.getElementById(result[1]).innerHTML = result[1] + " : " + result[2];
+		document.getElementById(result[1]).innerHTML = result[2];
 	}
 });
 
